0c7a48cd49f931b82eb113607be37a422ed844ea
Avik Samanta <97483380+Lojikick@users.noreply.github.com>
2024-07-25 21:28:14 -0700

    Added level loading screens and level freeing

--- testn/scripts/SceneManager.gd
+++ testn/scripts/SceneManager.gd
@@ -1,15 +1,15 @@
+#https://claude.ai/chat/164cb3e8-375b-46ae-b343-21190f3c1649
 extends Node
 
-#var next_level = null
+var next_level = null
 signal level_changed
-var current_level: Node2D
 var current_level_name = ""
+@onready var current_level = $game
 @onready var anim = $AnimationPlayer
 
 func _ready() -> void:
-	# Wait for one frame to ensure all nodes are properly added to the scene
 	await get_tree().process_frame
-	
+	print("We are here now")
 	current_level = get_node_or_null("game")
 	
 	if current_level:
@@ -26,32 +26,35 @@ func _ready() -> void:
 		print("Warning: game node not found")
 		print("Available children:", get_children())
 	
-	# Print the scene tree for debugging
 	print_scene_tree()
 
 func handle_level_changed(new_level_name: String):
 	print("Level changed to: ", new_level_name)
-	# Determine next level
-	var next_level
-
+	
+	var level_scene: PackedScene
+	
 	match new_level_name:
 		"Diner":
-			next_level = load("res://scenes/dialogue_ui.tscn").instantiate()
+			level_scene = preload("res://scenes/dialogue_ui.tscn")
 		"Booth":
-			next_level = load("res://scenes/game.tscn").instantiate()
+			level_scene = preload("res://scenes/game.tscn")
 		_:
 			print("Unknown level: ", new_level_name)
 			return
 	
-	if next_level:
-		add_child(next_level)
-		#draws underneath current level
-		#next_level.layer = -1
-		anim.play("fade_out")
-		next_level.connect("level_changed", handle_level_changed)
-		current_level.queue_free()
-		#Get rid of this
-		current_level = next_level
+	if level_scene:
+		next_level = level_scene.instantiate()
+		if next_level:
+			next_level.z_index = -1
+			add_child(next_level)
+			anim.play("fade_in")
+			
+			if next_level.has_signal("level_changed"):
+				next_level.connect("level_changed", handle_level_changed)
+		else:
+			print("Failed to instantiate new level")
+	else:
+		print("Level scene not found")
 
 func print_scene_tree():
 	print("Scene Tree:")
@@ -62,24 +65,15 @@ func _print_children(node: Node, indent: int):
 	for child in node.get_children():
 		_print_children(child, indent + 1)
 
-#func change_scene() -> void:
-	#get_tree().change_scene_to_file("res://scenes/dialogue_ui.tscn")
-# Called when the node enters the scene tree for the first time.
-
-#
-#avery frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-	#print("In the scene folder!")
-
-
-#EXPERIMENTAL
-#func _on_animation_player_animation_finished(anim_name):
-	#match anim_name:
-		#"fade_in":
-			#current_level.queue_free()
-			#current_level = next_level
-			#current.level.layer = 1
-			#next_level = null
-			#anim.play("fade_out")
-			#want player to see the current level till anim is complete
-			#fre animation
+func _on_animation_player_animation_finished(anim_name):
+	match anim_name:
+		"fade_in":
+			if current_level:
+				current_level.queue_free()
+			current_level = next_level
+			if current_level:
+				current_level.z_index = 1  # Set the level to the top
+			next_level = null
+			anim.play("fade_out")
+		"fade_out":
+			print("Fade out complete")
